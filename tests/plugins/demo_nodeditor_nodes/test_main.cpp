21740081791898b9d52f0b39a6fe35483c60adbc
samiavasil <samiavasil@gmail.com>
2026-08-11 06:26:50 +0300

    test (REQ-SW-PL-023, REQ-SW-PL-025): SampledData decode unit tests (normalized + physical)

--- tests/plugins/demo_nodeditor_nodes/test_main.cpp
+++ tests/plugins/demo_nodeditor_nodes/test_main.cpp
@@ -3,6 +3,7 @@
 
 #include "test_video_transform_ops.h"
 #include "test_stream_url_validator.h"
+#include "test_sampled_data.h"
 
 // Shared main for the demo_nodeditor_nodes video test classes. Each class is
 // declared in its own header so a single binary can run all of them through
@@ -22,6 +23,10 @@ int main(int argc, char *argv[])
     {
         TestStreamUrlValidator streamUrl;
         status |= QTest::qExec(&streamUrl, argc, argv);
+    }
+    {
+        SampledDataTest sampledData;
+        status |= QTest::qExec(&sampledData, argc, argv);
     }
     return status;
 }
